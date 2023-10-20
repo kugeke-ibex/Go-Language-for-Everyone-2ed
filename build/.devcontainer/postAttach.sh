b5195cc759633b7ecb047a5baa3071ece6e01b63
Kenjiro Kuge <kugetyan0211@gmail.com>
2023-10-20 13:48:18 +0000

    modify shellScript

--- build/.devcontainer/postAttach.sh
+++ build/.devcontainer/postAttach.sh
@@ -5,9 +5,12 @@ cd ..
 sudo chown -R devcontainer ~
 
 # install go development kit
+go install golang.org/x/tools/cmd/guru@latest
+go install golang.org/x/tools/cmd/gorename@latest
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/godoc@latest
 go install golang.org/x/lint/golint@latest
+go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/rogpeppe/godef@latest
 go install golang.org/x/vuln/cmd/govulncheck@latest
 go install github.com/go-delve/delve/cmd/dlv@master
@@ -17,5 +20,5 @@ go install github.com/josharian/impl@latest
 go install github.com/cweill/gotests/gotests@latest
 go install github.com/ramya-rao-a/go-outline@latest
 go install github.com/mdempsky/gocode@latest
-go install github.com/k0kubun/pp
+go install github.com/k0kubun/pp@latest
 go install honnef.co/go/tools/cmd/staticcheck@latest
