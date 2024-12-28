144af3636fb280afc8baf4d425eb1895fc2a9485
Kenjiro Kuge <kugetyan0211@gmail.com>
2024-12-28 15:39:29 +0000

    Chapter3-5

--- chapter-3/go-humanize/main.go
+++ chapter-3/go-humanize/main.go
@@ -18,4 +18,11 @@ func main () {
 
 	// ./main image.zip
 	// image.zip: 4.7 kB
+	// if bw, err := humanize.ParseBytes(maxBandWidth);
+	// err != nil {
+	// 	fmt.Println("Cannnot parse -max-bandwidth", err) // パースできない場合はエラー
+	// 	os.Exit(1)
+	// } else {
+	// 	conf.maxBandWidth
+	// }
 }
