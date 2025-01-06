0d2c09b24adec5d4d6f63f2747a1531f546eaf26
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-01-06 13:27:10 +0000

    Chapter5-3

--- chapter-5/reflect/Person/main.go
+++ chapter-5/reflect/Person/main.go
@@ -23,21 +23,19 @@ func main() {
 	rt := reflect.TypeOf(p)
 	rv := reflect.ValueOf(p)
 
-	if rv.Kind() == reflect.Ptr {
-		// reflect.Interfaceも同様
-		rv = rv.Elem()
-	}
-	if rt.Kind() == reflect.Ptr {
-		// reflect.Interfaceも同様
+	switch rt.Kind() {
+	case reflect.Interface, reflect.Ptr:
 		rt = rt.Elem()
+	}
+	switch rv.Kind() {
+	case reflect.Interface, reflect.Ptr:
+		rv = rv.Elem()
 	}
 
 	// fmt.Println(reflect.TypeOf(*p).Method(0))
 	for i := 0; i < rv.NumField(); i++ {
 		ft := rt.Field(i)
-		// i-th目の要素の型(reflect.StructField)
 		fv := rv.Field(i)
-		// i-th目の要素の値(reflect.Value)
 		if ft.PkgPath == "" {
 			fmt.Printf("ft(%d) -> %v\n", i, ft)
 			fmt.Printf("fv(%d) -> %v\n", i, fv.Interface())
