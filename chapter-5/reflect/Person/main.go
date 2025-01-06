ddc82ef003895fea3f646142c2ee54d589333760
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-01-06 13:43:30 +0000

    Chapter5-3

--- chapter-5/reflect/Person/main.go
+++ chapter-5/reflect/Person/main.go
@@ -18,6 +18,11 @@ func (p Person) printName() string {
 	return p.Name
 }
 
+type Point struct {
+	X int
+	Y int
+}
+
 func main() {
 	p := &Person{Name: "山田太郎", Age: 33}
 	rt := reflect.TypeOf(p)
@@ -35,10 +40,23 @@ func main() {
 	// fmt.Println(reflect.TypeOf(*p).Method(0))
 	for i := 0; i < rv.NumField(); i++ {
 		ft := rt.Field(i)
+		// i-th目の要素の型(reflect.StructField)
 		fv := rv.Field(i)
+		// i-th目の要素の値(reflect.Value)
 		if ft.PkgPath == "" {
 			fmt.Printf("ft(%d) -> %v\n", i, ft)
 			fmt.Printf("fv(%d) -> %v\n", i, fv.Interface())
 		}
 	}
+
+	point := Point{X:10, Y:5}
+	rvp := reflect.ValueOf(&point).Elem()
+	fmt.Println(rvp)
+	if !rvp.Field(0).CanSet() {
+		// エラー処理
+	}
+	if f := rvp.Field(0); f.CanSet() {
+		f.SetInt(100)
+	}
+	fmt.Println(rvp)
 }
